585a3e1d371c1dddd2262c370eba2810742e318a
mingletxt <zhuomingemail@gmail.com>
2013-05-08 10:41:07 +0800

    First commit

---
+++ src/com/mingle/DisplayMessageActivity.java
@@ -0,0 +1,33 @@
+package com.mingle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+/**
+ * User: mingle
+ * Date: 13-5-7
+ * Time: 下午9:54
+ * desc
+ */
+public class DisplayMessageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+
+}
